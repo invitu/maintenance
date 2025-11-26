8ac0798884d0f097c43f5f39e373c7167f9ad31a
Cyril VINH-TUNG <cyril@invitu.com>
2025-11-25 17:27:04 -1000

    [ADD] maintenance_equipment_next_request

---
+++ maintenance_equipment_next_request/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Maintenance Equipment Next Request",
+    "summary": """
+        Adds date field for next request on equipments""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "INVITU, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "depends": ["maintenance"],
+    "data": ["views/maintenance_views.xml"],
+}
---
+++ maintenance_equipment_next_request/models/maintenance.py
@@ -0,0 +1,26 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class MaintenanceEquipment(models.Model):
+    _inherit = "maintenance.equipment"
+
+    next_request_date = fields.Datetime(
+        compute="_compute_next_request_date",
+        store=True,
+    )
+
+    @api.depends("maintenance_ids.schedule_date", "maintenance_ids.stage_id")
+    def _compute_next_request_date(self):
+        for equipment in self:
+            next_request = equipment.maintenance_ids.search(
+                [
+                    ("equipment_id", "=", equipment.id),
+                    ("schedule_date", "!=", False),
+                    ("stage_id.done", "=", False),
+                ],
+                order="schedule_date asc",
+                limit=1,
+            )
+            equipment.next_request_date = next_request.schedule_date
